38083d8d986231db3b5e4a604e61d3a61bd1bf87
Bahtiar <zamkorong12@gmail.com>
2019-09-24 17:32:46 +0700

    Bebek Untuk Teman

---
+++ toki/versi lama/masukan dan keluaran/Bebek Untuk Teman.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int nilaiA, nilaiB;
+
+        scanf("%d",&nilaiA);
+        scanf("%d",&nilaiB);
+
+        printf("masing-masing %d\n",nilaiA/nilaiB);
+        printf("bersisa %d\n",nilaiA%nilaiB);
+
+    return 0;
+}
