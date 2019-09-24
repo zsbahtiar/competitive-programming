17a051285abf170a9727a9fcffa5123110906b70
Bahtiar <zamkorong12@gmail.com>
2019-09-24 16:44:42 +0700

    Aritmetika Dasar

---
+++ toki/versi lama/masukan dan keluaran/A Tambah B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, hasil;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    //sum a+b and printout
+    printf("%d\n",a+b);
+
+    return 0;
+}
