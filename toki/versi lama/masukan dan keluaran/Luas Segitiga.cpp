779e17e9ec14c7faba732aa281c30fcdca37bc5d
Bahtiar <zamkorong12@gmail.com>
2019-09-25 11:11:56 +0700

    luas segitiga with 2 desimal

---
+++ toki/versi lama/masukan dan keluaran/Luas Segitiga.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int alas, tinggi;
+    
+        scanf("%d",&alas);
+        scanf("%d",&tinggi);
+
+        printf("%.2f\n",0.5*alas*tinggi);
+
+    return 0;
+}
