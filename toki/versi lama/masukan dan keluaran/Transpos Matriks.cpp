2c941d3e9aa396b04692c2ad291ad15884a4ac96
Bahtiar <zamkorong12@gmail.com>
2019-09-25 11:23:06 +0700

    Transpos matriks

---
+++ toki/versi lama/masukan dan keluaran/Transpos Matriks.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d, e, f, g, h, i;
+
+        scanf("%d",&a);
+        scanf("%d",&b);
+        scanf("%d",&c);
+        scanf("%d",&d);
+        scanf("%d",&e);
+        scanf("%d",&f);
+        scanf("%d",&g);
+        scanf("%d",&h);
+        scanf("%d",&i);
+
+        printf("%d %d %d\n", a, d, g);
+        printf("%d %d %d\n", b, e, h);
+        printf("%d %d %d\n", c, f, i);
+
+    return 0;
+}
