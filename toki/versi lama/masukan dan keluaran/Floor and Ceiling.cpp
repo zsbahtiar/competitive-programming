6921cd5cdd789f97eb466c6030640d5debf4265c
Bahtiar <zamkorong12@gmail.com>
2019-09-26 15:24:15 +0700

    Floor and Ceiling.cpp
    Floor and Ceiling

---
+++ toki/versi lama/masukan dan keluaran/Floor and Ceiling.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    float nBil;
+
+        scanf("%f",&nBil);
+        printf("%.f %.f\n",floor(nBil),ceil(nBil));
+
+    return 0;
+}
