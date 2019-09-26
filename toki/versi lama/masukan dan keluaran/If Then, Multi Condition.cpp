c573d698a4094b7f0611de6075880f235cbc7a21
Bahtiar <zamkorong12@gmail.com>
2019-09-26 13:55:31 +0700

    If Then, Multi Condition

---
+++ toki/versi lama/masukan dan keluaran/If Then, Multi Condition.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int bilBul;
+
+        scanf("%d",&bilBul);
+        if(bilBul > 0 and bilBul%2==0){
+            printf("%d\n",bilBul);
+        }
+
+    return 0;
+}
