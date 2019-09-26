219eee1abb3f8ad6c4ad0c40c6a3a5a0c45fafa6
Bahtiar <zamkorong12@gmail.com>
2019-09-26 15:16:15 +0700

    Jarak Manhattan

---
+++ toki/versi lama/masukan dan keluaran/Jarak Manhattan.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x1,x2,y1,y2;
+
+        scanf("%d",&x1);
+        scanf("%d",&y1);
+        scanf("%d",&x2);
+        scanf("%d",&y2);
+
+        printf("%d\n",abs((x1-x2)+(y1-y2)));
+
+    return 0;
+}
