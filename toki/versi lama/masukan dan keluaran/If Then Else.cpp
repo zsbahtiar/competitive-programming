2308332867e001a507d93aa245e3aafd4ca80f8a
Bahtiar <zamkorong12@gmail.com>
2019-09-26 14:05:13 +0700

    If Then Else

---
+++ toki/versi lama/masukan dan keluaran/If Then Else.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int bilBUl;
+
+        scanf("%d",&bilBUl);
+        if(bilBUl>0){
+            printf("positif\n");
+        }else if(bilBUl<0){
+            printf("negatif\n");
+        }else{
+            printf("nol\n");
+        }
+
+    return 0;
+}
